ca0c83f9d6cbf866c6a905a66465d308176ff39f
jainvasu1 <vasubajaj2001@gmai.com>
2026-02-16 21:10:46 +0530

    top header

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -10,33 +10,55 @@ export default function Dashboard() {
     navigate("/login");
   };
 
+  const firstLetter = user?.email?.charAt(0).toUpperCase();
+
   return (
-  <div className="dashboard-layout">
+    <div className="dashboard-layout">
 
-  <div className="sidebar">
-    <div>
-      <h2 className="sidebar-title">Task Board</h2>
+      {/* SIDEBAR */}
+      <div className="sidebar">
+        <div className="sidebar-top">
+          <h2 className="sidebar-title">Task Board</h2>
+        </div>
 
-      <div className="sidebar-middle">
-        <p>My Activity</p>
-        <p>Backlog</p>
-        <p>Calendar</p>
-        <p>Dependencies</p>
+        <div className="sidebar-menu">
+          <p>My Activity</p>
+          <p>Backlog</p>
+          <p>Calendar</p>
+          <p>Dependencies</p>
+        </div>
+
+        <div className="sidebar-footer">
+          <p className="logout" onClick={handleLogout}>
+            Logout
+          </p>
+          <p className="made-by">Made by Vasudha</p>
+        </div>
+      </div>
+
+      {/* MAIN CONTENT */}
+      <div className="main-content">
+
+        {/* TOP HEADER */}
+        <div className="top-header">
+          <h2>Task Board / My Activity</h2>
+
+          <div className="header-right">
+            <div className="search-wrapper">
+  <span className="search-icon">🔍</span>
+  <input
+    type="text"
+    placeholder="Search..."
+    className="header-search"
+  />
+</div>
+            <div className="user-badge">
+              {firstLetter}
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
-
-    <div className="sidebar-bottom">
-      <button onClick={handleLogout} className="logout-btn">
-        Logout
-      </button>
-      <p className="made-by">Made by Vasudha</p>
-    </div>
-  </div>
-
-  <div className="main-content">
-  </div>
-
-</div>
-
   );
 }
